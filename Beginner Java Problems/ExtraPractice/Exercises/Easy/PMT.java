acd397f49220501794d58fdb3e77f63d525658cb
Michal Kaczorowski <codeflywin@gmail.com>
2025-10-01 19:37:10 -0500

    Directory Changes New Files

---
+++ Beginner Java Problems/ExtraPractice/Exercises/Easy/PMT.java
@@ -0,0 +1,10 @@
+package Easy;
+
+//Create a multiplication table for the number 1 multiplied up to the number 9
+public class PMT {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d * %d =  %d", 1, i, 1 * i).println();
+        }
+    }
+}
