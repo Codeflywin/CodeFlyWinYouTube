647f490f8fba348c68180821cb03c9a45c2c063e
Codeflywin <codeflywin@gmail.com>
2025-10-09 20:10:18 -0500

    New Files

---
+++ Beginner Java Problems/Intro to Java/Chapter2/ProblemNo13.java
@@ -0,0 +1,22 @@
+package Chapter2;
+
+import java.util.Scanner;
+
+public class ProblemNo13 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the monthly savings amount: ");
+        double savings = scan.nextInt();
+        double monthone = savings * (1 + 0.003125);
+        double monthtwo = (savings + monthone) * (1 + 0.003125);
+        double monththree = (savings + monthtwo) * (1 + 0.003125);
+        double monthfour = (savings + monththree) * (1 + 0.003125);
+        double monthfive = (savings + monthfour) * (1 + 0.003125);
+        double monthsix = (savings + monthfive) * (1 + 0.003125);
+        System.out.println("After the first month, the account value is: " + monthone );
+        System.out.println("After the second month, the account value is: " + monthtwo);
+        System.out.println("After the third month, the account value is: " + monththree);
+        System.out.println("After the sixth month, the account value is: " + monthsix);
+scan.close();
+    }
+}
---
+++ Beginner Java Problems/Intro to Java/Chapter2/ProblemNo14.java
@@ -0,0 +1,18 @@
+package Chapter2;
+
+import java.util.Scanner;
+
+public class ProblemNo14 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter weight in pounds: ");
+        double weightPounds = scan.nextDouble();
+        double weightKilograms = weightPounds * 0.45359237;
+        System.out.print("Enter height in inches: ");
+        double heightInches = scan.nextDouble();
+        double heightMeters =  heightInches * 0.0254;
+        double BMI = weightKilograms / Math.pow(heightMeters, 2);
+        System.out.println("BMI is " + BMI);
+        scan.close();
+    }
+}
