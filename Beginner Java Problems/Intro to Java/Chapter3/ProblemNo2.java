6636d12b440f1f266e9cd69b60d34126311ece76
Codeflywin <codeflywin@gmail.com>
2025-10-11 18:58:52 -0500

    File Changes

---
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo2.java
@@ -0,0 +1,20 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class ProblemNo2 {
+    public static void main(String[] args) {
+        int number1 = (int)(System.currentTimeMillis() % 10);
+        int number2 = (int)(System.currentTimeMillis() /10 % 10);
+        int number3 = (int)(System.currentTimeMillis() /10 / 10 % 10 );
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is: " + number1 + " * " + number2 + " * " + number3 + "? ");
+
+        int answer = scanner.nextInt();
+
+        System.out.println(number1 + " * " + number2 + " * " + number3 + " = " + answer + " is " +  (number1 * number2
+                * number3 == answer));
+            }
+}
---
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo3.java
@@ -0,0 +1,9 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class ProblemNo3 {
+    public static void main(String[] args) {
+
+    }
+}
