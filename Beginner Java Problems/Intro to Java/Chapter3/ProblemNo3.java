f49fc30b44a7fc32f5f069ab6c4073ab4411fc25
Codeflywin <codeflywin@gmail.com>
2025-10-11 19:49:36 -0500

    New File

--- Beginner Java Problems/Intro to Java/Chapter3/ProblemNo3.java
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo3.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class ProblemNo3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a, b, c, d, e, f: ");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double d = scanner.nextDouble();
+        double e = scanner.nextDouble();
+        double f = scanner.nextDouble();
+        double x = ((e * d) - (b * f)) / ((a * d) - (b * c));
+        double y = ((a * f) - (e * c)) / ((a * d) - (b * c));
+        double nosolution = (a * d) - (b * c);
+        if( nosolution != 0 )
+            System.out.println("x is " + x + " and y is " + y);
+         else
+            System.out.println("The equation has no solution.");
+            scanner.close();
+        }
     }
-}
