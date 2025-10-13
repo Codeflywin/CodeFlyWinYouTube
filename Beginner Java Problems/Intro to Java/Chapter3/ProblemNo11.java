f4d9e5625582a2230d052a211a2199cc2ae7d6f9
Codeflywin <codeflywin@gmail.com>
2025-10-13 17:22:40 -0500

    New File

--- Beginner Java Problems/Intro to Java/Chapter3/ProblemNo11.java
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo11.java
@@ -8,5 +8,34 @@ public class ProblemNo11 {
         System.out.print("Enter a month and a year: ");
         int month = input.nextInt();
         int year = input.nextInt();
-    }
-}
+        boolean leapYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+        if (month == 1)
+            System.out.println("January " + year + " has 31 days.");
+        else if (month == 2 && leapYear)
+            System.out.println("February " + year + " has 29 days.");
+        else if (month == 2)
+            System.out.println("February " + year + " has 28 days.");
+        else if (month == 3)
+            System.out.println("March " + year + " has 31 days.");
+        else if (month == 4)
+                System.out.println("April " + year + " has 30 days.");
+        else if (month == 5)
+                System.out.println("May " + year + " has 31 days.");
+        else if (month == 6)
+            System.out.println("June " + year + " has 30 days.");
+        else if (month == 7)
+                System.out.println("July " + year + " has 31 days.");
+        else if (month == 8)
+                System.out.println("August " + year + " has 31 days.");
+        else if (month == 9)
+                System.out.println("September " + year + " has 30 days.");
+        else if (month == 10)
+            System.out.println("October " + year + " has 31 days.");
+        else if (month == 11)
+                System.out.println("November " + year + " has 30 days.");
+        else if (month == 12)
+                System.out.println("December " + year + " has 31 days.");
+        input.close();
+            }
+        }
+
