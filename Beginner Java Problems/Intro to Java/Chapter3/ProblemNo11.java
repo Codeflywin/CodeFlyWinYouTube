dcc59511450e08f14d93202b6d07ca024cf2ef00
Codeflywin <codeflywin@gmail.com>
2025-10-12 20:55:43 -0500

    New File

---
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo11.java
@@ -0,0 +1,12 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class ProblemNo11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a month and a year: ");
+        int month = input.nextInt();
+        int year = input.nextInt();
+    }
+}
