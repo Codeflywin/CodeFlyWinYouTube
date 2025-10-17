ba9824efb887170d309748c5e11508b0ca743d18
Codeflywin <codeflywin@gmail.com>
2025-10-16 20:47:43 -0500

    File changes

--- Beginner Java Problems/Intro to Java/Chapter3/ProblemNo14.java
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo14.java
@@ -6,8 +6,6 @@ public class ProblemNo14 {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter 0 for heads, or 1 for tails to guess the flip of a coin: ");
         int number = input.nextInt();
-        if (number != 0 || number != 1)
-            System.out.println("I'm flipping a coin, but you did not enter 0 or 1, so you will have to re-enter.");
         //Math.random() will print a double between 0 and 1 (1 is not inclusive, in order for it to be 1, it needs to be
         // multiplied by 2 so the int truncates it and doesn't round it.
         int flip = (int) (Math.random() * 2);
