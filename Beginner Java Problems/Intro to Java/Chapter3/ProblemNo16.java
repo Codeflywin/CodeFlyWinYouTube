f2d992f8d340ddb98a64c4688bb41eab51b0c86f
Codeflywin <codeflywin@gmail.com>
2025-10-16 21:46:11 -0500

    File changes

---
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo16.java
@@ -0,0 +1,14 @@
+package Chapter3;
+
+public class ProblemNo16 {
+    public static void main (String[] args) {
+
+        // Math random will generate a double between 0 and 1 (1 is not inclusive), so if we multiply it by 50,
+        // the highest value it can give us is 49,the width of the square is 50, therefore since we have
+        //values from -25 to 25, we must subtract 25 to make the max x value 24, or -24
+        int x = (int)  ((Math.random() * 50) - 25);
+        // For y values 0.99 multiplied by 150, gives us a max value of 148.5 so we need to subtract 75
+        int y = (int)  ((Math.random() * 150) - 75);
+        System.out.println("The coordinates are: " + x +"," +y);
+    }
+}
---
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo17.java
@@ -0,0 +1,33 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class ProblemNo17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("scissor (0), rock (1), paper (2): ");
+        int hand = input.nextInt();
+        int computerhand = (int) (Math.random() * 3);
+
+        if  (hand == 0 && computerhand == 0)
+            System.out.println("The computer is scissors. You are scissors. It is a draw.");
+        else if (hand == 1 && computerhand == 1)
+            System.out.println("The computer is rock. You are rock. It is a draw.");
+        else if (hand == 2 && computerhand == 2)
+            System.out.println("The computer is paper. You are paper. It is a draw.");
+        else if (hand == 0 && computerhand == 1)
+            System.out.println("The computer is rock. You are scissors. You lose.");
+        else if (hand == 0 && computerhand == 2)
+            System.out.println("The computer is paper. You are scissors. You win.");
+        else if (hand == 1 && computerhand == 0)
+            System.out.println("The computer is scissors. You are rock. You win.");
+        else if (hand == 1 && computerhand == 2)
+            System.out.println("The computer is paper. You are rock. You lose.");
+        else if (hand == 2 && computerhand == 0)
+            System.out.println("The computer is scissors. You are paper. You lose.");
+        else if (hand == 2 && computerhand == 1)
+            System.out.println("The computer is scissors. You are rock. You win.");
+            input.close();
+        }
+    }
+
