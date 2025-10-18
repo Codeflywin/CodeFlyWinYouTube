fcd72ed861c29f14a40e79e7cf5be634ee04d5a1
Codeflywin <codeflywin@gmail.com>
2025-10-18 11:20:31 -0500

    File changes

---
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo21.java
@@ -0,0 +1,30 @@
+package Chapter3;
+
+import java.util.Scanner;
+public class ProblemNo21 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the year: (e.g., 2012): ");
+        int year = scan.nextInt();
+        System.out.println("Enter the month 1-12: ");
+        int m= scan.nextInt();
+        System.out.println("Enter the day of the month: 1-31: ");
+        int q = scan.nextInt();
+        int century = year % 100;
+        int j = year / 100;
+        int h = (q+ ((26 * (m + 1)) / 10) + century + (century / 4) + ( j / 4) + (5 * j));
+        System.out.println("The day of the week is: " );
+
+        switch (h % 7) {
+            case 0: System.out.println("Monday"); break;
+            case 1: System.out.println("Tuesday"); break;
+            case 2: System.out.println("Wednesday"); break;
+            case 3: System.out.println("Thursday"); break;
+            case 4: System.out.println("Friday"); break;
+            case 5: System.out.println("Saturday"); break;
+            case 6: System.out.println("Sunday"); break;
+        }
+        scan.close();
+    }
+}
+
