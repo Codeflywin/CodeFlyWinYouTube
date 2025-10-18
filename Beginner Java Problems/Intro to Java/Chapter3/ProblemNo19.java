534e46e6d7e115152951732115b7a0c9bbd898d6
Codeflywin <codeflywin@gmail.com>
2025-10-18 10:31:42 -0500

    New File

---
+++ Beginner Java Problems/Intro to Java/Chapter3/ProblemNo19.java
@@ -0,0 +1,21 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class ProblemNo19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the three edges of a triangle in order to calculate the perimeter:");
+        System.out.println("Edge a: ");
+        double a = input.nextDouble();
+        System.out.println("Edge b: ");
+        double b = input.nextDouble();
+        System.out.println("Edge c: ");
+        double c = input.nextDouble();
+        if ( a + b > c || a + c > b || b + c > a)
+            System.out.println("The perimeter equals: " + (a + b + c) );
+        else
+            System.out.println("The input is invalid.");
+
+    }
+}
