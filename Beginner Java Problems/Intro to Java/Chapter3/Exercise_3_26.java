310b6c4dd2c029fc90a340fa0fcb4341b31ca707
Codeflywin <codeflywin@gmail.com>
2025-10-18 20:07:35 -0500

    New File

---
+++ Beginner Java Problems/Intro to Java/Chapter3/Exercise_3_26.java
@@ -0,0 +1,27 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class Exercise_3_26 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.print("Is " + number + " divisible by 4 and 5? ");
+        if ( number % 4 == 0 && number % 5 == 0)
+            System.out.print(" true");
+        else
+            System.out.print(" false");
+        System.out.print(" Is " + number + " divisible by 4 or 5? ");
+        if (number % 4 == 0 || number % 5 == 0)
+            System.out.print(" true");
+        else
+            System.out.print(" false");
+        System.out.print(" Is " + number + " divisible by 4 or 5, but not both? ");
+        if (number % 4 == 0 ^ number % 5 == 0)
+            System.out.print(" true");
+        else
+            System.out.print(" false");
+        input.close();
+    }
+}
